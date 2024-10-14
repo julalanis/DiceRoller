a1771b106be044786d6ed8dd3063b7ad7a994297
Julie Alanis <136264672+julalanis@users.noreply.github.com>
2024-10-14 16:53:06 -0500

    Update Die.java
    Added new data fields + get/set methods and new for loop contents to add modifier.

--- src/Die.java
+++ src/Die.java
@@ -1,6 +1,9 @@
 public class Die extends DiceRoller {
     private int numDie;
     private int valueDie;
+    private int modifier;
+    private int result;
+    private int total;
 
     public Die(){
     }
@@ -18,6 +21,18 @@ public class Die extends DiceRoller {
         return valueDie;
     }
 
+    public int getModifier(){
+        return modifier;
+    }
+
+    public int getResult(){
+        return result;
+    }
+
+    public int getTotal(){
+        return total;
+    }
+
     public void setNumDie(int numDie){
         this.numDie = numDie;
     }
@@ -26,8 +41,31 @@ public class Die extends DiceRoller {
         this.valueDie = valueDie;
     }
 
+    public void setModifier(int modifier){
+        this.modifier = modifier;
+    }
+
+    public void setResult(int result){
+        this.result = result;
+    }
+
+    public void setTotal(int total){
+        this.total = total;
+    }
+
     public String dieToString(){
         return "This successfully copied the input values of numDie: " + numDie + " and valueDie: " + valueDie;
     }
 
+    public void rollDie(int x, int y){
+        for (int i = 0; i < x; i++){
+            result = (int)(Math.random() * y);
+            total += result;
+            System.out.println("You rolled a " + result);
+
+        }
+        System.out.println("The total is: " + total);
+        int newTotal = total + modifier;
+        System.out.println("With modifier of " + modifier + ", the new total is: " + newTotal);
+    }
 }
