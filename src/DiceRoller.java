b365c23461d24c9059b3c6d25eeeaa19c76ab93c
Julie Alanis <136264672+julalanis@users.noreply.github.com>
2024-10-13 22:56:17 -0500

    DiceRoller.java
    Rewrote entire DiceRoller class, work in progress

--- src/DiceRoller.java
+++ src/DiceRoller.java
@@ -1,58 +1,24 @@
 import java.util.Scanner;
+
 public class DiceRoller {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Please enter a die value or type 'end': ");
+        Scanner scanner = new Scanner(System.in);
+        Die die = new Die();
 
-        String chosenDie = input.nextLine();
-        boolean isDie = true;
+        System.out.println("Please enter number of dice you would like to roll: ");
+        int numDie = (scanner.nextInt());
+        die.setNumDie(numDie);
 
-        if (chosenDie.equals("end")) {
-        	isDie = false;
-            System.out.print("Ending die rolls. Thank you.");
-            System.exit(0); 
-        }
 
-        while (isDie){
-        	if (chosenDie.equals("d4")){
-            int result = (int)(Math.random() * 4);
-            System.out.println("You roll a d4. The result is " + result);
-            System.out.print("Please enter another die: ");
-            chosenDie = input.nextLine();
-        } else if (chosenDie.equals("d6")){
-            int result = (int)(Math.random() * 6);
-            System.out.println("You roll a d6. The result is " + result);
-            System.out.print("Please enter another die: ");
-            chosenDie = input.nextLine();
-        } else if (chosenDie.equals("d8")){
-            int result = (int)(Math.random() * 8);
-            System.out.println("You roll a d8. The result is " + result);
-            System.out.print("Please enter another die: ");
-            chosenDie = input.nextLine();
-        } else if (chosenDie.equals("d10")){
-            int result = (int)(Math.random() * 10);
-            System.out.println("You roll a d4. The result is " + result);
-            System.out.print("Please enter another die: ");
-            chosenDie = input.nextLine();
-        } else if (chosenDie.equals("d12")){
-            int result = (int)(Math.random() * 12);
-            System.out.println("You roll a d4. The result is " + result);
-            System.out.print("Please enter another die: ");
-            chosenDie = input.nextLine();
-        } else if (chosenDie.equals("d20")){
-            int result = (int)(Math.random() * 20);
-            System.out.println("You roll a d4. The result is " + result);
-            System.out.print("Please enter another die: ");
-            chosenDie = input.nextLine();
-        } else {
-            System.out.print("Please enter a valid die or type 'end': ");
-        	chosenDie = input.nextLine();
-        	if (chosenDie.equals("end")) {
-        		isDie = false;
-        		System.out.print("Ending die rolls. Thank you.");
-        		System.exit(0);
-        		}
-        	}
-        }
+        System.out.println("Please enter value of die (2, 4, 6, 8, 10, 12, 20): ");
+        int valueDie = scanner.nextInt();
+        die.setValueDie(valueDie);
+
+        System.out.println(die.dieToString());
+
+
+
+
+
     }
 }
