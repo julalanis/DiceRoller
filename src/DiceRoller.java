5cde38789f7d9017fabda8e712a2c42926af5f8a
Julie Alanis <136264672+julalanis@users.noreply.github.com>
2024-10-14 16:52:30 -0500

    Update DiceRoller.java
    Added modifier to roll

--- src/DiceRoller.java
+++ src/DiceRoller.java
@@ -5,20 +5,20 @@ public class DiceRoller {
         Scanner scanner = new Scanner(System.in);
         Die die = new Die();
 
-        System.out.println("Please enter number of dice you would like to roll: ");
+        System.out.print("Please enter number of dice you would like to roll: ");
         int numDie = (scanner.nextInt());
         die.setNumDie(numDie);
 
 
-        System.out.println("Please enter value of die (2, 4, 6, 8, 10, 12, 20): ");
+        System.out.print("Please enter value of die (2, 4, 6, 8, 10, 12, 20): ");
         int valueDie = scanner.nextInt();
         die.setValueDie(valueDie);
 
-        System.out.println(die.dieToString());
+        System.out.print("Please enter any modifiers: ");
+        int modifier = scanner.nextInt();
+        die.setModifier(modifier);
 
-
-
-
+        die.rollDie(numDie, valueDie);
 
     }
 }
